b60cfc5d6ee78c8550bcf00e33da5771559e1ad8
AKHILCHANDRAN19 <hdcbmktr20@gmail.com>
2026-09-20 08:06:36 +0530

    Update __init__.py

--- ​pdf_tools/__init__.py
+++ ​pdf_tools/__init__.py
@@ -1,11 +1,11 @@
-from pdf_tools.page_numbers import stamp_page_numbers
 from pdf_tools.compressor import compress_pdf_file
-from pdf_tools.merger import merge_pdf_list
-from pdf_tools.splitter import execute_split, parse_split_pattern
 from pdf_tools.image_converter import (
-    extract_pdf_pages_as_images,
     compile_images_into_pdf,
+    extract_pdf_pages_as_images,
 )
+from pdf_tools.merger import merge_pdf_list
+from pdf_tools.page_numbers import stamp_page_numbers
+from pdf_tools.splitter import execute_split, parse_split_pattern
 
 __all__ = [
     "stamp_page_numbers",
