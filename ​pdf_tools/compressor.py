f67e376578064d17e8ffbf072447df1399848d01
AKHILCHANDRAN19 <hdcbmktr20@gmail.com>
2026-09-20 08:18:42 +0530

    Delete ​pdf_tools directory

--- ​pdf_tools/compressor.py
+++
@@ -1,8 +0,0 @@
-import pymupdf
-
-
-def compress_pdf_file(input_path: str, output_path: str) -> str:
-  doc = pymupdf.open(input_path)
-  doc.save(output_path, garbage=4, deflate=True)
-  doc.close()
-  return output_path
