d561e474350ac0723aed59de3cbca672725c3625
AKHILCHANDRAN19 <hdcbmktr20@gmail.com>
2026-09-20 08:01:30 +0530

    Refactor imports and update __all__ in __init__.py

--- ​pdf_tools/__init__.py
+++ ​pdf_tools/__init__.py
@@ -1,15 +1,18 @@
 from pdf_tools.page_numbers import stamp_page_numbers
 from pdf_tools.compressor import compress_pdf_file
 from pdf_tools.merger import merge_pdf_list
-from pdf_tools.splitter import execute_split
-from pdf_tools.image_converter import extract_pdf_pages_as_images, compile_images_into_pdf
+from pdf_tools.splitter import execute_split, parse_split_pattern
+from pdf_tools.image_converter import (
+    extract_pdf_pages_as_images,
+    compile_images_into_pdf,
+)
 
 __all__ = [
     "stamp_page_numbers",
     "compress_pdf_file",
     "merge_pdf_list",
     "execute_split",
+    "parse_split_pattern",
     "extract_pdf_pages_as_images",
     "compile_images_into_pdf",
 ]
-
