17380942f1d7765ed638a8c3c33af66d71f73023
AKHILCHANDRAN19 <hdcbmktr20@gmail.com>
2026-09-20 08:50:57 +0530

    Update import statements and __all__ exports

--- pdf_tools/__init__.py
+++ pdf_tools/__init__.py
@@ -1,11 +1,8 @@
-from pdf_tools.compressor import compress_pdf_file
-from pdf_tools.image_converter import (
-    compile_images_into_pdf,
-    extract_pdf_pages_as_images,
-)
-from pdf_tools.merger import merge_pdf_list
-from pdf_tools.page_numbers import stamp_page_numbers
-from pdf_tools.splitter import execute_split, parse_split_pattern
+from .compressor import compress_pdf_file
+from .image_converter import compile_images_into_pdf, extract_pdf_to_images
+from .merger import merge_pdf_list
+from .page_numbers import stamp_page_numbers
+from .splitter import execute_split, parse_split_pattern
 
 __all__ = [
     "stamp_page_numbers",
@@ -13,7 +10,6 @@ __all__ = [
     "merge_pdf_list",
     "execute_split",
     "parse_split_pattern",
-    "extract_pdf_pages_as_images",
+    "extract_pdf_to_images",
     "compile_images_into_pdf",
 ]
-
